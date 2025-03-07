a7fd8ddf26719eeca374d496097ddacb9258fde1
Kyrylo <fresh.js.ftd.@gmail.com>
2025-03-07 18:47:20 +0200

    Refactor multer middlewares on server

--- server/src/index.js
+++ server/src/index.js
@@ -6,17 +6,27 @@ const cors = require('cors');
 require('./dbMongo/mongoose');
 const router = require('./router');
 const controller = require('./socketInit');
+const multerErrorHandler = require('./handlerError/multerErrorHandler');
 const handlerError = require('./handlerError/handler');
+const { PROD_FILES_PATH, DEV_FILES_PATH } = require('./constants');
+const env = process.env.NODE_ENV || 'development';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/public', express.static('public'));
+app.use(
+  '/public',
+  express.static(env === 'production' ? PROD_FILES_PATH : DEV_FILES_PATH),
+);
 app.use(router);
+
+app.use(multerErrorHandler);
 app.use(handlerError);
 
 const server = http.createServer(app);
-server.listen(PORT, () =>  console.log(`Example app listening on port ${PORT}!`));
+server.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`),
+);
 controller.createConnection(server);
--- server/src/utils/fileUpload.js
+++ server/src/utils/fileUpload.js
@@ -1,78 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-const ServerError = require('../errors/ServerError');
 const env = process.env.NODE_ENV || 'development';
-const devFilePath = path.resolve(__dirname, '..', '..', '..', 'public/images');
+const { DEV_FILES_PATH, PROD_FILES_PATH } = require('../constants');
 
-const filePath = env === 'production'
-  ? '/var/www/html/images/'
-  : devFilePath;
+const devImagesPath = path.resolve(DEV_FILES_PATH, 'images');
+const devContestsPath =  path.resolve(DEV_FILES_PATH, 'contests');
 
-if (!fs.existsSync(filePath)) {
-  fs.mkdirSync(filePath, {
+const imagesFilePath = env === 'production' ? `${PROD_FILES_PATH}/images` : devImagesPath;
+const contestsFilePath = env === 'production' ? `${PROD_FILES_PATH}/images` : devContestsPath;
+
+if(!fs.existsSync(imagesFilePath)) {
+  fs.mkdirSync(imagesFilePath, {
     recursive: true,
   });
 }
 
-const storageContestFiles = multer.diskStorage({
-  destination (req, file, cb) {
-    cb(null, filePath);
+if(!fs.existsSync(contestsFilePath)) {
+  fs.mkdirSync(contestsFilePath, {
+    recursive: true,
+  });
+}
+
+const storageImagesFiles = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, imagesFilePath);
   },
-  filename (req, file, cb) {
+  filename(req, file, cb) {
     cb(null, Date.now() + file.originalname);
   },
 });
 
-const uploadAvatars = multer({ storage: storageContestFiles }).single('file');
-const uploadContestFiles = multer({ storage: storageContestFiles }).array(
-  'files', 3);
-const updateContestFile = multer({ storage: storageContestFiles }).single(
-  'file');
-const uploadLogoFiles = multer({ storage: storageContestFiles }).single(
-  'offerData');
+const storageContestFiles = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, contestsFilePath);
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-module.exports.uploadAvatar = (req, res, next) => {
-  uploadAvatars(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
+const multerImagesStorage = multer({ storage: storageImagesFiles });
+const multerContestStorage = multer({ storage: storageContestFiles });
 
-module.exports.uploadContestFiles = (req, res, next) => {
-  uploadContestFiles(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
-
-module.exports.updateContestFile = (req, res, next) => {
-  updateContestFile(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
-
-module.exports.uploadLogoFiles = (req, res, next) => {
-  uploadLogoFiles(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
-
+module.exports.uploadAvatar = multerImagesStorage.single('file');
+module.exports.uploadContestFiles = multerContestStorage.array('files', 3);
+module.exports.updateContestFile = multerContestStorage.single('file');
+module.exports.uploadLogoFile = multerContestStorage.single('offerData');
